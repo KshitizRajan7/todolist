c798ec6799b051c0296e8fc41424db6b021ff4cf
kshitizrajan7 <kshitizrajandev@gmail.com>
2024-07-20 17:18:42 +0545

    deployment of the application

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ *  @type {import('next').NextConfig}  
+*/
+const nextConfig = {
+    basePath: '/todolist', // Replace with your GitHub repository name
+    assetPrefix: '/todolist/',
+    // output:'export',
+    // distDir:'dist',
+    // images:{
+    //     unoptimized: true,
+    // }
+};
 
 export default nextConfig;
